9f8dfe6e7458619574a5896e62037e0ddb97d4b4
Andrew Breckenridge <asbreckenridge@me.com>
2018-03-11 05:51:25 -0700

    Implement lost animation in Me

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import ReactWindowSized from 'react-window-size'
 import FourOhFour from './components/FourOhFour.js'
 import Me from './components/Me.js'
 
+
+
 class App extends Component {
   render() {
     switch (true) { // o_o "pattern matching", they call this. Javascript 😢
       case (this.props.windowWidth > 575):
-        return <p style={{display: 'flex', justifyContent: 'center'}}>screen too wide</p>
+        return (<p style={{display: 'flex', justifyContent: 'center'}}>
+          screen too wide
+        </p>)
       // TODO: why does this look fine on the iPhone but not on macOS at this height? 715 breaks on desktop
       // case (this.props.windowHeight < 715):
       //   return <p style={{display: 'flex', justifyContent: 'center'}}>screen too short</p>
--- src/components/Me.js
+++ src/components/Me.js
@@ -2,21 +2,77 @@ import React, { Component } from 'react';
 
 const backgroundImageUrl = 'https://78.media.tumblr.com/3c9a8417a347d806520acc60267a3dac/tumblr_nkap4jjcuq1twprc3o1_1280.jpg'
 
+const AnimationState = Object.freeze({
+  notYet: 0,
+  animating: 1,
+  done: 2,
+})
+
 export default class extends Component {
-  componentDidMount() {
-    console.log(this.refs.hi.offsetWidth)
+  state = {
+    animating: AnimationState.notYet,
+    overlayAlpha: 0,
   }
+
   render() {
+    console.log('rendering')
     return (
-      <div ref='hi' style={styles.background}>
-        <a style={styles.lost}>u lost?</a>
+        <div style={styles.background}>
+        <a style={styles.lost} onClick={this.handleClick.bind(this)}>u lost?</a>
         <p style={styles.name}>A N D R W</p>
+        <div style={styles.whiteOverlay(this.state)} />
       </div>
     )
+  }
+
+  handleClick() {
+    console.log(this.state)
+    if (this.state.animating === AnimationState.animating) { return }
+
+    this.setState({ animating: AnimationState.animating })
+    this.animate()
+  }
+
+  // Callee is responsible for only calling this once
+  animate() {
+    const opacityJumps = [30, 40, 50, 60, 70, 80, 90, 100]
+    var i = 0;
+    var parentThis = this;
+    const intervalHandle = setInterval(() => {
+      console.log(parentThis.state)
+      parentThis.setState({ overlayAlpha: opacityJumps[i] })
+      i++;
+      if (i === opacityJumps.length) {
+        clearInterval(intervalHandle)
+        parentThis.setState({ animating: AnimationState.done, overlayAlpha: 0 })
+      }
+    }, 250)
+
   }
 }
 
 const styles = {
+  whiteOverlay: (state) => {
+    console.log('called', state)
+
+    // Ask Case how to store this state
+    if (state.animating === 1) {
+      console.log('lolyo')
+      console.log(state.overlayAlpha / 100)
+      return {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        background: 'white',
+        opacity: state.overlayAlpha / 100,
+      }
+    } else {
+      console.log('fux')
+      return { display: 'none' }
+    }
+  },
   background: {
     backgroundImage: `url(${backgroundImageUrl})`,
     backgroundSize: 'cover',
