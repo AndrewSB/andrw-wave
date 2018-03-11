b412a8f062b44b70489bcbc7d9798fba2ad155d2
Andrew Breckenridge <asbreckenridge@me.com>
2018-03-11 04:38:08 -0700

    components/Me: Looks good

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-// import meBg from '../../public/me-bg.jpg'
+
+const backgroundImageUrl = 'https://78.media.tumblr.com/3c9a8417a347d806520acc60267a3dac/tumblr_nkap4jjcuq1twprc3o1_1280.jpg'
 
 export default class extends Component {
+  componentDidMount() {
+    console.log(this.refs.hi.offsetWidth)
+  }
   render() {
     return (
-      <div style={styles.background}>
-        <p>hi</p>
+      <div ref='hi' style={styles.background}>
+        <a style={styles.lost}>u lost?</a>
+        <p style={styles.name}>A N D R W</p>
       </div>
     )
   }
@@ -13,9 +18,9 @@ export default class extends Component {
 
 const styles = {
   background: {
-    backgroundImage: 'url(../../public/me-bg.jpg)',
+    backgroundImage: `url(${backgroundImageUrl})`,
     backgroundSize: 'cover',
-    backgroundPosition: 'bottom center',
+    backgroundPosition: 'top center',
     position: 'absolute',
     top: 0,
     bottom: 0,
@@ -23,9 +28,19 @@ const styles = {
     right: 0,
   },
   lost: {
-
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '10vh',
+    color: '#DF976C',
+    fontFamily: "Andale Mono, AndaleMono, monospace",
+    fontSize: '3vw',
   },
   name: {
-
+    color: '#176EA8',
+    fontFamily: "Andale Mono, AndaleMono, monospace",
+    fontSize: '8vw',
+    marginLeft: '5vw',
+    marginTop: '30vh',
   },
 }
