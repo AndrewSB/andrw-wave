1a52837626918082a2298238833e23440f1f5442
Andrew Breckenridge <asbreckenridge@me.com>
2018-03-13 10:46:01 -0700

    Add a better help message to the screen too narrow text

--- src/components/Device.js
+++ src/components/Device.js
@@ -6,10 +6,18 @@ import Me from './Me.js'
 class Device extends Component {
   render() {
     switch (true) { // o_o "pattern matching", they call this. Javascript 😢
-      case (this.props.windowWidth > 575):
-        return (<p style={{display: 'flex', justifyContent: 'center'}}>
-          screen too wide
-        </p>)
+    case (this.props.windowWidth > 575):
+      const centerTextStyle = {display: 'flex', justifyContent: 'center'}
+      return (
+        <div>
+          <p style={centerTextStyle}>
+	          your screen is too wide
+          </p>
+          <p style={centerTextStyle}>
+	          make your screen narrower and the page will render -- this is a work in progress
+          </p>
+        </div>
+      )
       // TODO: why does this look fine on the iPhone but not on macOS at this height? 715 breaks on desktop
       // case (this.props.windowHeight < 715):
       //   return <p style={{display: 'flex', justifyContent: 'center'}}>screen too short</p>
