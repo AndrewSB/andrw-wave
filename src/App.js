96bd79f998c10f35104010b0e7a71009c226dc1d
Andrew Breckenridge <asbreckenridge@me.com>
2020-01-19 00:53:29 -0800

    Me: make the page responsive for mobile

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Device from './components/Device'
 require('viewport-units-buggyfill').init()
 
 class App extends Component {
   render() {
-    setTimeout(() => {window.scrollTo(100, 1)}, 1000)
     return (
-      <HashRouter>
+      <BrowserRouter>
         <Device />
-      </HashRouter>
+      </BrowserRouter>
     )
   }
 }
--- src/components/Me.js
+++ src/components/Me.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 
 const backgroundImageUrl = 'https://78.media.tumblr.com/3c9a8417a347d806520acc60267a3dac/tumblr_nkap4jjcuq1twprc3o1_1280.jpg'
 
@@ -14,15 +13,18 @@ export default class extends Component {
     animating: AnimationState.notYet,
     overlayAlpha: 0,
   }
-
+  
+  // this is kind of a shit check, but media-queries dont work well in React and this seems to work
+  isMobile = ['Mobile', 'iPhone', 'Android', 'Windows Phone']
+    .some((candidateString) => navigator.userAgent.indexOf(candidateString) !== -1)
+  
   render() {
-    console.log('rendering')
     return (
         <div style={styles.background}>
-          <div style={styles.lostContainer}>
+          <div style={styles.lostContainer(this.isMobile)}>
             <a style={styles.lost} onClick={this.handleClick.bind(this)}>u lost?</a>
           </div>
-          <div style={styles.nameContainer}>
+          <div style={styles.nameContainer(this.isMobile)}>
             <p style={styles.name}>A N D R W</p>
           </div>
         <div style={styles.whiteOverlay(this.state)} />
@@ -31,7 +33,6 @@ export default class extends Component {
   }
 
   handleClick() {
-    console.log(this.state)
     if (this.state.animating === AnimationState.animating) { return }
 
     this.setState({ animating: AnimationState.animating })
@@ -58,12 +59,7 @@ export default class extends Component {
 
 const styles = {
   whiteOverlay: (state) => {
-    console.log('called', state)
-
-    // Ask Case how to store this state
     if (state.animating === 1) {
-      console.log('lolyo')
-      console.log(state.overlayAlpha / 100)
       return {
         position: 'absolute',
         top: 0,
@@ -74,7 +70,6 @@ const styles = {
         opacity: state.overlayAlpha / 100,
       }
     } else {
-      console.log('fux')
       return { display: 'none' }
     }
   },
@@ -88,20 +83,25 @@ const styles = {
     left: 0,
     right: 0,
   },
-  lostContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: '9.5vh',
+  lostContainer: (isMobile) => {
+    return {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginRight: '3vw',
+      marginTop: isMobile ? '8.5vh' : '10vh',
+    }
   },
   lost: {
     color: '#DF976C',
     fontFamily: "Andale Mono, AndaleMono, monospace",
-    fontSize: '3vw',
+    fontSize: '4vw',
   },
-  nameContainer: {
-    marginLeft: '5vw',
-    marginTop: '30vh',
+  nameContainer: (isMobile) => {
+    return {
+      marginLeft: isMobile ? '4vw' : '5vw',
+      marginTop: isMobile ? '25vh' : '31vh',
+    }
   },
   name: {
     color: '#176EA8',
