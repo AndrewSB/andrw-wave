8414da1f6f8888167521b23df3feaa3dfa11f733
Andrew Breckenridge <asbreckenridge@me.com>
2020-04-10 03:35:06 -0700

    Add typing effect to PokemonDialogBox

--- src/components/PokemonDialogBox.tsx
+++ src/components/PokemonDialogBox.tsx
@@ -1,4 +1,6 @@
 import React, { Component, useState, useEffect } from 'react';
+import Typed from 'react-typed';
+import calculateWordDimensions from '../calculate-text-dimensions';
 
 interface Props {
   styles?: string,
@@ -6,47 +8,75 @@ interface Props {
 };
 
 var dialogBoxState = [
-  {height: 30, node: (<>?</>)},
-  {height: 44, node: (<>this is a fun art project - not the portfolio you might expect</>)},
-  {height: 30, node: (<>
+  {height: 30, node: '?'},
+  {height: 30, node: (`
     here are some links to click:
     <span> </span>
     <a href='https://github.com/AndrewSB'>github</a>
     <span> </span>
     <a href='https://linkedin.com/in/ndrww'>linkedin</a>
     <span> </span>
-  </>)},
-  {height: 63, node: <>if you’ve made it this far, I would love to get to know you. send an e-mail to <a href='mailto:asbreckenridge@me.com'>asbreckenridge@me.com</a> with an answer to “What’s your favorite color?”</>},
-  {height: 44, node: <>note: no one ever does this. I would be soOoOo thrilled if you did.</>},
-  {height: 30, node: <><a>remember to make time to be lost</a></>}
+  `)},
+  {height: 63, node: 'if you’ve made it this far, I would love to get to know you. send an e-mail to <a href=\'mailto:asbreckenridge@me.com\'>asbreckenridge@me.com</a> with an answer to \“What’s your favorite color?\"'},
+  {height: 44, node: 'note: no one ever does this. I would be soOoOo thrilled if you did.'},
+  {height: 30, node: '<a>remember to make time to be lost</a>'}
 ]
 
 const PokemonDialogBox: React.FC<Props> = (props) => {
   const [boxState, setBoxState] = useState(0);
-  const [height, setHeight] = useState(16);
+  const [boxHeight, setBoxHeight] = useState(18);
+
+  const [typed, setTyped] = useState(undefined);
+
+  const [textDoneTyping, setTextDoneTyping] = useState(true);
+  const [skipTypingCommand, setSkipTypingCommand] = useState(true);
+
   useEffect(
-    () => setHeight(document.getElementById('box-content').getBoundingClientRect().height),
+    () => {
+      const box = document.getElementById('box-content');
+      const boxDimensions = calculateWordDimensions(dialogBoxState[boxState].node, box.getBoundingClientRect().width);
+      setBoxHeight(boxDimensions.height)
+    },
     [boxState]
   )
 
   return <div
     onClick={() => {
-      const nextState = (boxState + 1) % dialogBoxState.length;
-      if (nextState == 0) props.pushLostPage();
-      else setBoxState(nextState);
+      if (!textDoneTyping) {
+        typed.reset();
+        setSkipTypingCommand(true);
+      } else {
+        const nextState = (boxState + 1) % dialogBoxState.length;
+        if (nextState == 0) props.pushLostPage();
+        else {
+          setBoxState(nextState);
+          typed.reset();
+        }
+      }
     }}
     className={'box ' + (boxState == 0 ? 'inactive' : 'active')}>
-      <div id='box-content'>{dialogBoxState[boxState].node}</div>
-      <i />
+      <div id='box-content'>
+        <Typed
+          typedRef={typed => setTyped(typed)}
+          preStringTyped={() => setTextDoneTyping(false)}
+          onStringTyped={() => {
+            setSkipTypingCommand(false)
+            setTextDoneTyping(true)
+          }}
+          strings={[skipTypingCommand ? `\`${dialogBoxState[boxState].node}\`` : dialogBoxState[boxState].node]}
+          typeSpeed={9}
+          showCursor={false} />
+      </div>
+      {textDoneTyping && <i />}
   <style jsx>{`
     .box {
       ${props.styles}
+      height: ${boxHeight + 8}px;
       font-size: 14px;
       font-family: "Press Start 2P", Arial, sans-serif;
       border-radius: 2px;
       padding: 8px;
       line-height: 16px;
-      height: ${height + 8}px;
       margin: auto;
       background: white;
       border: 1px solid white;
@@ -69,14 +99,19 @@ const PokemonDialogBox: React.FC<Props> = (props) => {
       height: 0;
       border: 5px solid transparent;
       border-top-color: black;
-      margin-top: 23px;
       animation: shake-vertical 1s ease-in-out infinite alternate;
       float: right;
     }
 
     @keyframes shake-vertical {
-      from { margin-top: 0px; }
-      to { margin-top: 2px; }
+      from {
+        margin-top: 0px;
+        padding-top: 0px;
+      }
+      to {
+        margin-top: -2px;
+        padding-top: 2px;
+      }
     }
   `}</style>
   </div>
