9537760dee9ef1d09062832b82454631b180a28e
Andrew Breckenridge <asbreckenridge@me.com>
2020-04-30 00:33:32 -0700

    tracking: move page events to their own script tag for better nextjs parsability

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -53,9 +53,11 @@ export const _AnalyticsHead: React.FC<{path: string}> = ({path}) => {
       <script defer async dangerouslySetInnerHTML={{__html: dedent`
         !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
         analytics.load("xoFId4OzdmQYD6HQWyKiZKyZjhfszGup");
-        analytics.page({url: document.location.href, uuid: window.tuuid});
         }}();
       `}} />
+      <script defer dangerouslySetInnerHTML={{__html:
+        `analytics.page({url: document.location.href, uuid: window.tuuid, path: '${path}'});`
+      }} />
       <script defer dangerouslySetInnerHTML={{__html:
         `fetch("https://enc7ni3p7gol81k.m.pipedream.net?path=${path}&uuid=" + window.tuuid)`
       }} />
