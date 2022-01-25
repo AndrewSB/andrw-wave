8b209bfe67e19955aed2a6e8bb37ed05733f7221
Andrew Breckenridge <asbreckenridge@me.com>
2022-01-24 23:24:17 -0800

    pages/404: migrate from react-optimized-image -> next/image

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,34 +1,39 @@
-import React from 'react';
-import Img from 'react-optimized-image';
-import FakePhonePanel from '../components/FakePhonePanel';
-import { AnalyticsHead } from './_app';
-import styles from './404.module.css';
+import React from "react";
+import Image from "next/image";
+import FakePhonePanel from "../components/FakePhonePanel";
+import { AnalyticsHead } from "./_app";
+import styles from "./404.module.css";
 
-const backgroundImageUrl = 'http://78.media.tumblr.com/081bf739ed5bacf9268c6951c67cc13b/tumblr_np01gl81Mb1qbzzgco1_r1_540.png'
+const backgroundImageUrl =
+  "http://78.media.tumblr.com/081bf739ed5bacf9268c6951c67cc13b/tumblr_np01gl81Mb1qbzzgco1_r1_540.png";
 const backgroundImageUrlPallete = {
-  'dominant': 'rgb(194, 194, 219)',
-  'lightPink': 'rgba(227, 182, 251, 0.9)',
-  'darkPink': 'rgba(150, 124, 170)',
-  'lightGrey': 'rgb(182, 201, 207)',
+  dominant: "rgb(194, 194, 219)",
+  lightPink: "rgba(227, 182, 251, 0.9)",
+  darkPink: "rgba(150, 124, 170)",
+  lightGrey: "rgb(182, 201, 207)",
 };
-const okHandImageUrl = 'https://78.media.tumblr.com/c0f430296f0ec59343bb11e6e2a38a25/tumblr_o1q0o5T23V1uf5j8co1_250.gif'
+const okHandImageUrl =
+  "https://78.media.tumblr.com/c0f430296f0ec59343bb11e6e2a38a25/tumblr_o1q0o5T23V1uf5j8co1_250.gif";
 
 export default function Custom404() {
   return (
     <>
-      <AnalyticsHead path='/404' />
-      <FakePhonePanel showingWidth={800} customStyle={`
+      <AnalyticsHead path="/404" />
+      <FakePhonePanel
+        showingWidth={800}
+        customStyle={`
         background: ${backgroundImageUrlPallete.lightPink};
         position: absolute;
         top: 0;
         right: 0;
         left: 0;
         bottom: 0;
-      `} >
-        <div className={styles.background} >
-          <div className='title'>
+      `}
+      >
+        <div className={styles.background}>
+          <div className="title">
             <p>4</p>
-            <Img src={require('../../public/ok-hand.gif')} />
+            <Image src="/ok-hand.gif" width={250} height={250} />
             <p>4</p>
           </div>
         </div>
@@ -45,9 +50,9 @@ export default function Custom404() {
         p {
           font-size: 66px;
           font-family: Andale Mono, AndaleMono, monospace;
-          color: #E2019B;
+          color: #e2019b;
         }
       `}</style>
     </>
-  )
-};
+  );
+}
