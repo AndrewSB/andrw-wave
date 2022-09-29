c243e76fcf71d7b1fad8575de9ee7cf511fc1ca5
Andrew Breckenridge <asbreckenridge@me.com>
2022-09-29 01:49:53 -0700

    PokemonDialogBox: further improve box sizing

--- src/components/PokemonDialogBox.tsx
+++ src/components/PokemonDialogBox.tsx
@@ -39,24 +39,23 @@ var dialogBoxState = [
   },
 ];
 
-/// ignoringValuesOfHeight: this is a total hack to get around the fact that useResizeObserver fires before
-/// the dangerouslySetInnerHTML takes effect for the opacity 0 measurement div... i wonder if there's a
-/// a better way to do this. maybe i'll figure it out in 2023
-const useSize = (target, ignoringValuesOfHeight) => {
+const useSize = (target, shouldIgnore: (number) => boolean) => {
   const [size, setSize] = React.useState<DOMRectReadOnly | null>();
 
   React.useLayoutEffect(() => {
     const s = target.current.getBoundingClientRect();
-    if (s.height !== ignoringValuesOfHeight) {
+    if (!shouldIgnore(s.height)) {
       setSize(s);
     }
-  }, [target, ignoringValuesOfHeight]);
+  }, [target, shouldIgnore]);
 
   // Where the magic happens
   useResizeObserver(target, (entry) => {
     const s = entry.contentRect;
-    if (s.height !== ignoringValuesOfHeight) {
-      setSize(entry.contentRect);
+    if (!shouldIgnore(s.height)) {
+      setSize(s);
+    } else {
+      console.log("i");
     }
   });
   return size;
@@ -66,16 +65,51 @@ const PokemonDialogBox: React.FC<Props> = (props) => {
   const [boxState, setBoxState] = useState(0);
 
   const measureRef = React.useRef(null);
-  const measureBoxSize = useSize(measureRef, 128);
+  const measureBoxIgnorePredicate = useCallback(() => false, []);
+  const measureBoxSize = useSize(measureRef, measureBoxIgnorePredicate);
 
   const [typed, setTyped] = useState<any>(null);
   const dialogBoxRef = React.useRef(null);
-  const dialogBoxSize = useSize(dialogBoxRef, 128);
+  /// dialogBoxIgnorePredicate: this is a total hack to get around the fact that useResizeObserver fires before
+  /// the dangerouslySetInnerHTML takes effect for the opacity 0 measurement div... i wonder if there's a
+  /// a better way to do this. maybe i'll figure it out in 2023
+  /// basically what's happening is the measurement div is still at a `?` sized width, then we switch the text in
+  /// it, so it thinks it needs to be _super_ tall, then one render loop later we set the measurement div to the
+  /// correct width
+  /// i don't like that single render loop flash tho
+  const dialogBoxIgnorePredicate = useCallback(
+    () => measureBoxSize?.height === 128 && boxState === 1,
+    [measureBoxSize, boxState]
+  );
+  const dialogBoxSize = useSize(dialogBoxRef, dialogBoxIgnorePredicate);
 
-  // console.log(dialogBoxSize?.width, measureBoxSize?.height);
+  console.log(dialogBoxSize, measureBoxSize, boxState);
 
   const [textDoneTyping, setTextDoneTyping] = useState(true);
   const [skipTypingCommand, setSkipTypingCommand] = useState(true);
+
+  const handleDialogPress = useCallback(() => {
+    if (!textDoneTyping) {
+      typed.reset();
+      setSkipTypingCommand(true);
+    } else {
+      const nextState = (boxState + 1) % dialogBoxState.length;
+      if (nextState == 0) props.pushLostPage();
+      else {
+        setBoxState(nextState);
+        props.onNewText(dialogBoxState[nextState].node);
+        typed.reset();
+      }
+    }
+  }, [boxState, textDoneTyping, typed, props]);
+
+  useEffect(() => {
+    window.onkeydown = (event) => {
+      if (event.code === "Space") {
+        handleDialogPress();
+      }
+    };
+  }, [handleDialogPress]);
 
   return (
     <React.Fragment>
@@ -83,9 +117,11 @@ const PokemonDialogBox: React.FC<Props> = (props) => {
         ref={measureRef}
         style={{
           position: "absolute",
-          opacity: 1,
+          opacity: 0,
+          // background: "red",
           left: -2000,
-          width: dialogBoxSize?.width ?? 1000,
+          overflowWrap: "break-word",
+          width: (dialogBoxSize?.width ?? 0) - 8,
           fontSize: 14,
           lineHeight: "16px",
           fontFamily: '"Press Start 2P", Arial, sans-serif',
@@ -93,20 +129,7 @@ const PokemonDialogBox: React.FC<Props> = (props) => {
         dangerouslySetInnerHTML={{ __html: dialogBoxState[boxState].node }}
       />
       <div
-        onClick={() => {
-          if (!textDoneTyping) {
-            typed.reset();
-            setSkipTypingCommand(true);
-          } else {
-            const nextState = (boxState + 1) % dialogBoxState.length;
-            if (nextState == 0) props.pushLostPage();
-            else {
-              setBoxState(nextState);
-              props.onNewText(dialogBoxState[nextState].node);
-              typed.reset();
-            }
-          }
-        }}
+        onClick={handleDialogPress}
         className={"box " + (boxState == 0 ? "inactive" : "active")}
       >
         <div ref={dialogBoxRef} id="box-content" className="cursor-default">
@@ -130,13 +153,13 @@ const PokemonDialogBox: React.FC<Props> = (props) => {
         <style jsx>{`
           .box {
             ${props.styles}
-            height: ${(measureBoxSize?.height ?? 0) + 22}px;
+            height: ${(measureBoxSize?.height ?? 0) + 25}px;
             font-size: 14px;
             font-family: "Press Start 2P", Arial, sans-serif;
             border-radius: 2px;
             padding: 8px;
+            overflow-wrap: break-word;
             line-height: 16px;
-            margin: auto;
             background: white;
             border: 1px solid white;
             box-shadow: 0 1px 0 1px black, inset 0 1px 0 1px black,
@@ -166,8 +189,8 @@ const PokemonDialogBox: React.FC<Props> = (props) => {
               padding-top: 0px;
             }
             to {
-              margin-top: -2px;
-              padding-top: 2px;
+              margin-top: -4px;
+              padding-top: 4px;
             }
           }
         `}</style>
