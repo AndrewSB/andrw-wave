40c4199be03a08ed1d091d2a70ef16cb17c60895
Andrew Breckenridge <asbreckenridge@me.com>
2023-10-24 15:53:07 -0700

    pages/emir: make fullscreen

--- src/pages/emir.tsx
+++ src/pages/emir.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import AnalyticsHead from "../components/AnalyticsHead";
+import Head from "next/head";
 
 export default function Emir() {
   return (
     <React.Fragment>
+      <Head>
+        <title>head of hamas</title>
+      </Head>
       <AnalyticsHead path="/emir" incrementVists={false} />
-      <video src="/Chadposting Average Turk.mp4" autoPlay />
+      <video
+        src="/Chadposting Average Turk.mp4"
+        autoPlay={true}
+        loop
+        playsInline
+        controls={true}
+        style={{
+          objectFit: "scale-down",
+          width: "100vw",
+          height: "100vh",
+          position: "fixed",
+          top: 0,
+          left: 0,
+        }}
+      />
     </React.Fragment>
   );
 }
