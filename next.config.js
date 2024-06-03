a019f00606482fb364d27a7f7f81cb865f770aa4
Andrew Breckenridge <asbreckenridge@me.com>
2024-06-02 20:05:28 -0700

    pages/rescue: add alias for duress

--- next.config.js
+++ next.config.js
@@ -1,46 +1,52 @@
 module.exports = {
   images: {
-    domains: ['i.scdn.co'],
+    domains: ["i.scdn.co"],
   },
   async redirects() {
     return [
       {
-        source: '/newmac',
-        destination: 'https://raw.githubusercontent.com/AndrewSB/dotfiles/master/script/new-mac.sh',
+        source: "/newmac",
+        destination:
+          "https://raw.githubusercontent.com/AndrewSB/dotfiles/master/script/new-mac.sh",
         permanent: true,
       },
       {
-        source: '/lucid',
-        destination: 'http://i.imgur.com/hJmweFb.jpg',
+        source: "/lucid",
+        destination: "http://i.imgur.com/hJmweFb.jpg",
         permanent: true,
       },
       {
-        source: '/linkedin',
-        destination: 'http://linkedin.com/in/ndrww',
+        source: "/linkedin",
+        destination: "http://linkedin.com/in/ndrww",
         permanent: true,
       },
-    ]
+      {
+        source: "/duress",
+        destination: "/rescue",
+        permanent: true,
+      },
+    ];
   },
-     async headers() {
-      return [
-        {
-          source: "/fonts/MSBee-Bold.woff2",
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "public, max-age=31536000, immutable",
-            },
-          ],
-        },
-        {
-          source: "/fonts/MSBee-Regular.woff2",
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "public, max-age=31536000, immutable",
-            },
-          ],
-        },
-      ];
-    },
-}
+  async headers() {
+    return [
+      {
+        source: "/fonts/MSBee-Bold.woff2",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/fonts/MSBee-Regular.woff2",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
+};
